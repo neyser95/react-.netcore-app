c7e07b1c750770fa440b048c7046b3dad885dce6
neyser <neyserj.zana@protonmail.com>
2021-09-12 11:00:34 -0400

    refactor and starting to use typescript

--- client-app/src/app/layouts/App.tsx
+++ client-app/src/app/layouts/App.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import './App.css';
 import axios from 'axios';
 import { Header, List } from 'semantic-ui-react';
+import { Activity } from './../models/activity';
 const {useState, useEffect} = React;
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
   
   useEffect(() => {
-    axios.get('http://localhost:5000/api/activities').then(response => setActivities(response.data));
+    axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => setActivities(response.data));
   }, []);
 
   return (
     <div>
       <Header as='h2' icon='users' content={'Reactivites'}/>
         <List>
-          {activities.map((a: any) => (
+          {activities.map(a => (
             <List.Item key={a.id}> 
               {a.title}
             </List.Item>
